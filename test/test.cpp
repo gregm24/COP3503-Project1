25b6dbb2a4d132583e5fc19f0dbb4cf0fcaded91
gregm24 <144722295+gregm24@users.noreply.github.com>
2025-09-20 14:14:09 -0400

    Add 3 Catch2 test cases

--- test/test.cpp
+++ test/test.cpp
@@ -1,53 +1,84 @@
 #include <catch2/catch_test_macros.hpp>
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cstdlib>
 
-// uncomment and replace the following with your own headers
-// #include "AVL.h"
-
+#include "AVL.h"
 using namespace std;
 
-// the syntax for defining a test is below. It is important for the name to be unique, but you can group multiple tests with [tags]. A test can have [multiple][tags] using that syntax.
-TEST_CASE("Example Test Name - Change me!", "[flag]"){
-	// instantiate any class members that you need to test here
-	int one = 1;
+// Gregory Myers - 74883466
 
-	// anything that evaluates to false in a REQUIRE block will result in a failing test 
-	REQUIRE(one == 0); // fix me!
+TEST_CASE("Incorrect Commands", "[flag]"){
+	AVL tree;
 
-	// all REQUIRE blocks must evaluate to true for the whole test to pass
-	REQUIRE(false); // also fix me!
+	REQUIRE(tree.runCommand("") == false);
+	REQUIRE(tree.runCommand("insert \"A11y\" 45679999") == false);
+	REQUIRE(tree.runCommand("insert") == false);
+	REQUIRE(tree.runCommand("remove string") == false);
+	REQUIRE(tree.runCommand("print inorder tooMany") == false);
 }
 
-TEST_CASE("Test 2", "[flag]"){
-	// you can also use "sections" to share setup code between tests, for example:
-	int one = 1;
+TEST_CASE("Insert & Rotations", "[flag]") {
 
-	SECTION("num is 2") {
-		int num = one + 1;
-		REQUIRE(num == 2);
-	};
+	SECTION("right rotation") {
+		AVL t;
+		t.insert(30);
+		t.insert(20);
+		t.insert(10);
+		vector<int> expected{10, 20, 30};
+		REQUIRE(t.inorder() == expected);
+	}
 
-	SECTION("num is 3") {
-		int num = one + 2;
-		REQUIRE(num == 3);
-	};
+	SECTION("left rotation") {
+		AVL t;
+		t.insert(10);
+		t.insert(20);
+		t.insert(30);
+		std::vector<int> expected{10, 20, 30};
+		REQUIRE(t.inorder() == expected);
+	}
 
-	// each section runs the setup code independently to ensure that they don't affect each other
+	SECTION("left-right rotation") {
+		AVL t;
+		t.insert(30);
+		t.insert(10);
+		t.insert(20);
+		std::vector<int> expected{10, 20, 30};
+		REQUIRE(t.inorder() == expected);
+	}
+
+	SECTION("right-left rotation") {
+		AVL t;
+		t.insert(10);
+		t.insert(30);
+		t.insert(20);
+		std::vector<int> expected{10, 20, 30};
+		REQUIRE(t.inorder() == expected);
+	}
 }
 
-// you must write 5 unique, meaningful tests for credit on the testing portion of this project!
+TEST_CASE("BST Insert", "[flag]"){
+	AVL t;
+	vector<int> expected, actual;
 
-// the provided test from the template is below.
+	for (int i = 0; i < 100; i++) {
+		int randomInput = rand();
+		if (count(expected.begin(), expected.end(), randomInput) == 0) {
+			expected.push_back(randomInput);
+			t.insert(randomInput);
+		}
+	}
 
-TEST_CASE("Example BST Insert", "[flag]"){
-	/*
-		MyAVLTree tree;   // Create a Tree object
-		tree.insert(3);
-		tree.insert(2);
-		tree.insert(1);
-		std::vector<int> actualOutput = tree.inorder();
-		std::vector<int> expectedOutput = {1, 2, 3};
-		REQUIRE(expectedOutput.size() == actualOutput.size());
-		REQUIRE(actualOutput == expectedOutput);
-	*/
+	for (int j = 0; j < 10; j++) {
+		int idx = rand() % expected.size();
+		t.remove(expected[idx]);
+		expected.erase(expected.begin() + idx);
+	}
+
+	actual = t.inorder();
+	REQUIRE(expected.size() == actual.size());
+	sort(expected.begin(), expected.end());
+	REQUIRE(expected == actual);
 }
